396c165d5264a0bbec8fb7e667a3173f1e341397
Benny Chean <benny.chean@gmail.com>
2021-07-28 20:44:47 -0500

    Added Flask Login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import requests
 from app import app
-from .forms import PokeForm
+from .forms import PokeForm, LoginForm, RegisterForm
+from .models import User
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -30,3 +32,43 @@ def pokemons():
             error_string="Please check for error"
             render_template("pokemon.html.j2",form=form, error=error_string)
     return render_template("pokemon.html.j2", form=form)
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        try:
+            new_user_data = {
+                "first_name": form.first_name.data.title(),
+                "last_name": form.last_name.data.title(),
+                "email": form.email.data.lower(),
+                "password": form.password.data
+                }
+            new_user_object = User()
+            new_user_object.from_dict(new_user_data)
+        except:
+            error_string="There was a problem creating your account. Please try again."
+            return render_template('register.html.j2', form=form, error=error_string)
+        return redirect(url_for('login'))
+    return render_template('register.html.j2', form=form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data.lower()
+        password = form.password.data
+        u = User.query.filter_by(email=email).first()
+        if u is not None and u.check_hashed_password(password):
+            login_user(u)
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+    return render_template("login.html.j2", form=form)
+
+@app.route('/logout', methods = ['GET'])
+@login_required
+def logout():
+    if current_user is not None:
+        logout_user()
+        return redirect(url_for('login'))
